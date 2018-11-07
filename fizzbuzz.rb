af2f795ed4106116ece8bf73c1fb12844d8413ec
Kaitlyn Middlebrook <kmiddlebrook2000@gmail.com>
2018-11-07 05:19:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,5 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(i)
+  fizzbuzz(3) = "Fizz"
+  fizz_5 = fizzbuzz(5)
+  fizz_15 = fizzbuzz(15)
+end  
